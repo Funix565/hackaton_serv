39a62488f99368c421467ecddd233bb6847416cc
Funix565 <denkin.dmitriy@gmail.com>
2021-03-20 17:22:53 +0200

    json concrete article

--- app.py
+++ app.py
@@ -55,105 +55,35 @@ def show() :
                 "status": "not null",
                 "news_date": str(rec[0]),
                 "contents": rec[1],
-                "news_text": rec[2], # only title and desc
-                "author": rec[3],
                 "news_pic_url": rec[4]
             }
-            #print(json.dumps(result))
             string_res += json.dumps(result) + "<br>"
-        #return "Suc"
         string_res += "]"
         return string_res
 
 # write get for concrete article
 
+@app.route('/news/<int:id_news>/<string:contents>/')
+def publish(id_news, contents) :
+    cur = con.cursor()
+    cur.execute("SELECT id_news, news_date, contents, news_text, author, news_pic_url FROM news WHERE id_news = %s AND contents = %s", ((id_news), (contents)))
+    rows = cur.fetchone()
+    if (rows == None) :
+        result = {
+            "status" : "Fail"
+        }
+        return json.dumps(result)
+    else :
+        result = {
+            "status" : "Success",
+            "news_date" : str(rows[1]),
+            "contents" : rows[2],
+            "news_text" : rows[3],
+            "author" : rows[4],
+            "news_pic_url" : rows[5]
+        }
+        return json.dumps(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-# #!flask/bin/python
-# from flask import Flask
-# import psycopg2
-# import json
-
-# con = psycopg2.connect(
-#   database="hack", 
-#   user="sergres", 
-#   password="password",
-#   host="204.2.63.23",
-#   port="24192"
-# )
-
-# print("Database opened successfully")
-
-# app = Flask(__name__)
-
-# @app.route('/login/<string:phone>/<string:password>/')
-# def login(phone, password):
-#     cur = con.cursor()
-#     cur.execute("SELECT phone, password FROM customer WHERE phone = %s AND password = %s", ((phone), (password)))
-#     rows = cur.fetchone()
-#     if (rows == None) :
-#         result = {
-#             "status" : "Fail"
-#         }
-#     else :
-#         result = {
-#             "status" : "Success"
-#         }
-#     return json.dumps(result)
-
-#     # get full news
-
-
-# @app.route('/news/list/')
-# def show() :
-#     print('News list')
-#     cur = con.cursor()
-#     cur.execute("SELECT news_date, contents, news_text, author, news_pic_url FROM news LIMIT 10")
-#     rows = cur.fetchall()
-#     if (rows == []) :
-#         result = {
-#             "status" : "null"
-#         }
-#         return json.dumps(result)
-#     else :
-#         string_res = "[<br>"
-#         for rec in rows :
-#             result = {
-#                 "status": "not null",
-#                 "news_date": str(rec[0]),
-#                 "contents": rec[1],
-#                 "news_pic_url": rec[4]
-#             }
-#             string_res += json.dumps(result) + "<br>"
-#         string_res += "]"
-#         return string_res
-
-# # write get for concrete article
-
-# # @app.route('/news/<int:id_news>/<string:contents>/')
-# # def publish(id_news, contents) :
-# #     cur = con.cursor()
-# #     cur.execute("SELECT id_news, contents FROM news WHERE id_news = %s AND contents = %s", ((id_news), (contents)))
-# #     rows = cur.fetchone()
-# #     if (rows == None) :
-# #         print('None')
-# #         result = {
-# #             "status" : "Fail"
-# #         }
-# #     else:
-# #         print('I in')
-# #         result = {
-# #             "status" : "Success"
-# #         }
-# #     con.commit()
-# #     con.close()
-# #     return json.dumps(result)
-
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
